051c3a9871a35d1e4376c9f7253f509167d838bd
vezhindan <ivan_tamayo82132@elpoli.edu.co>
2020-05-22 17:13:47 -0500

    TestJaCoCo

---
+++ src/main/java/com/hanoli/testProyect/TestJaCoCo4/App.java
@@ -0,0 +1,16 @@
+package com.hanoli.testProyect.TestJaCoCo4;
+
+import  org.apache.commons.io.FileUtils;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
